2a72833484c366cc87e7a6a29bc58edc85288364
Arief Akhmad Fauzi <sekolah.arief@gmail.com>
2023-05-03 07:21:33 +0700

    init

---
+++ kmean.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+# Read data
+df = pd.read_csv("input/Mall_Customers.csv")
+# Info
+df.info()
+df.head()
+df.drop(["CustomerID"], axis = 1, inplace=True)
+df.info()
+
+from mpl_toolkits.mplot3d import Axes3D
+sns.set_style("white")
+fig = plt.figure(figsize=(20,10))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(df.Age, df["Annual Income (k$)"], df["Spending Score (1-100)"], c='blue', s=60)
+ax.view_init(30, 185)
+plt.xlabel("Age")
+plt.ylabel("Annual Income (k$)")
+ax.set_zlabel('Spending Score (1-100)')
+plt.show()
+
+from sklearn.cluster import KMeans
+wcss = []
+for k in range(1,11):
+    kmeans = KMeans(n_clusters=k, init="k-means++")
+    kmeans.fit(df.iloc[:,1:])
+    wcss.append(kmeans.inertia_)
+plt.figure(figsize=(12,6))    
+plt.grid()
+plt.plot(range(1,11),wcss, linewidth=2, color="red", marker ="8")
+plt.xlabel("K Value")
+plt.xticks(np.arange(1,11,1))
+plt.ylabel("WCSS")
+plt.show()
+
+km = KMeans(n_clusters=5)
+clusters = km.fit_predict(df.iloc[:,1:])
+df["label"] = clusters
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+fig = plt.figure(figsize=(20,10))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(df.Age[df.label == 0], df["Annual Income (k$)"][df.label == 0], df["Spending Score (1-100)"][df.label == 0], c='blue', s=60)
+ax.scatter(df.Age[df.label == 1], df["Annual Income (k$)"][df.label == 1], df["Spending Score (1-100)"][df.label == 1], c='red', s=60)
+ax.scatter(df.Age[df.label == 2], df["Annual Income (k$)"][df.label == 2], df["Spending Score (1-100)"][df.label == 2], c='green', s=60)
+ax.scatter(df.Age[df.label == 3], df["Annual Income (k$)"][df.label == 3], df["Spending Score (1-100)"][df.label == 3], c='orange', s=60)
+ax.scatter(df.Age[df.label == 4], df["Annual Income (k$)"][df.label == 4], df["Spending Score (1-100)"][df.label == 4], c='purple', s=60)
+ax.view_init(30, 185)
+plt.xlabel("Age")
+plt.ylabel("Annual Income (k$)")
+ax.set_zlabel('Spending Score (1-100)')
+plt.show()
